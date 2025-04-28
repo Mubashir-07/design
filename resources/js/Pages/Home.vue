0860bb4da742a0020e28bec4ec3054029145aa20
Mubashir-07 <mubashirmhd34@protonmail.com>
2025-04-28 22:50:54 +0530

    Routes and layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,6 @@
+
 <template>
-    <div>
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    </div>
+    <h1  class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </template>
